92f24fed109e6fd10cc2d5dfb2adfe51ebe2cb47
sourav-biswas04 <sb303180@gmail.com>
2025-03-19 09:56:57 +0530

    Reference variable using programme

---
+++ Reference.java
@@ -0,0 +1,13 @@
+public class Reference {
+    void res1 (){
+        System.out.println("Sourav");
+    }
+    void res2(){
+        System.out.println("Biswas");
+    }
+    public static void main(String[] args) {
+        Reference b = new Reference();
+        b.res1();
+        b.res2();
+    }
+}
