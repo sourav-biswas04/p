99eab7b6c2c73d7c06c8407d346d17bc5ee3512d
sourav-biswas04 <sb303180@gmail.com>
2025-03-19 09:37:44 +0530

    add this file

---
+++ Mul.java
@@ -0,0 +1,16 @@
+public class Mul {
+    static void res()
+    {
+        int a=23;
+        int b=33;
+        int c=4;
+        int d=6;
+        int e=a*b*c*d;
+        System.out.println(e);
+    }
+    public static void main(String[] args) {
+       
+        System.out.println("value is:");
+        res();
+    }
+}
