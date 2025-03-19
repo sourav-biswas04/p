d1d7fe87d4711fe0cbdac3454f900b8a6f8a6be9
sourav-biswas04 <sb303180@gmail.com>
2025-03-19 09:48:38 +0530

    Method the parameter as non static with in the class

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    void res(int a, int b){
+        int c=a-b;
+        System.out.println(c);
+    }
+    public static void main(String[] args) {
+        Demo a =new Demo();
+        a.res(22,10);
+    }
+}
